45979a7ab8087c497f1d9f0d1ee72728b6cdf5be
Stephen Kuenzli <skuenzli@qualimente.com>
2016-07-04 21:38:21 -0700

    Generate the addressbook client from the Apache quickstart archtype.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Overview #
+
+The addressbook-client project demonstrates usage of the AddressBook server API and leverages the documented contract examples for testing.
+
---
+++ src/main/java/com/qualimente/training/addressbook/client/App.java
@@ -0,0 +1,13 @@
+package com.qualimente.training.addressbook.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
